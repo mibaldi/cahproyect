e093ddb3b766a74992ff937f1db03a80854725f1
Mikel Balduciel Diaz <mibaldi2@gmail.com>
2018-01-14 13:59:58 +0100

    rx and fix last child

--- app/src/main/java/com/mibaldi/cah/managers/GameFirebaseManager.kt
+++ app/src/main/java/com/mibaldi/cah/managers/GameFirebaseManager.kt
@@ -8,37 +8,65 @@ import javax.inject.Inject
 import javax.inject.Singleton
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
-
+import io.reactivex.Observable
+import io.reactivex.Observer
 
 
 @Singleton
 class GameFirebaseManager @Inject constructor(){
 
-    var database = FirebaseDatabase.getInstance()
+
     companion object {
+        var database = FirebaseDatabase.getInstance()
         val refPlayers = "jugadores"
         val refRounds = "turnos"
+        var gameRef = database.getReference("juegos")
     }
-    var gameRef = database.getReference("juegos")
 
-    fun newGame(game : Game, res: (String) -> Unit){
+
+    fun newGame(game : Game) : Observable<String>{
         val key = gameRef.push().key
         gameRef.child(key).setValue(game.toGameFirebase())
-        res(key)
+        return Observable.create<String> {
+            it.onNext(key)
+            it.onComplete()
+        }
     }
 
-    fun addPlayer(key: String,player: Player) {
+    fun addPlayer(key: String,player: Player,subscriber: Observer<Boolean>) {
         gameRef.child(key).child("${refPlayers}/${player.username}").setValue(player.toPlayerFirebase())
+                .addOnCompleteListener{ task ->
+                    subscriber.onNext(task.isSuccessful)
+                }
     }
 
     fun startRound(gameKey: String) {
         gameRef.child(gameKey).child(refRounds).orderByKey().limitToLast(1).addListenerForSingleValueEvent (object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                gameRef.child(gameKey).child(refRounds).child(dataSnapshot.key).updateChildren(mapOf(Pair("estado",0)))
+                dataSnapshot.children.forEach {
+                    gameRef.child(gameKey).child(refRounds).child(it.key).child("estado").setValue(1)
+                }
+
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
                 //Handle possible errors.
+            }
+        })
+    }
+    fun isGamePrepared(gameKey: String,subscriber: Observer<Boolean>){
+        gameRef.child(gameKey).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                when(dataSnapshot.child("estado").value){
+                    1L -> {
+                        gameRef.child(gameKey).removeEventListener(this)
+                        subscriber.onComplete()
+                    }
+                    else -> subscriber.onError(Error("Juego no preparado"))
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                subscriber.onError(Error(databaseError.message))
             }
         })
     }
--- app/src/main/java/com/mibaldi/cah/ui/presenters/newGame/NewGamePresenter.kt
+++ app/src/main/java/com/mibaldi/cah/ui/presenters/newGame/NewGamePresenter.kt
@@ -1,30 +1,84 @@
 package com.mibaldi.cah.ui.presenters.newGame
 
 import android.util.Log
+import com.google.android.gms.tasks.Task
 import com.mibaldi.cah.base.presenters.activities.BasePresenter
 import com.mibaldi.cah.data.models.Game
 import com.mibaldi.cah.data.models.Player
 import com.mibaldi.cah.managers.GameFirebaseManager
 import com.mibaldi.cah.router.Router
 import com.mibaldi.cah.ui.views.NewGameContract
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
 class NewGamePresenter @Inject constructor(val router: Router, val gameManager: GameFirebaseManager): BasePresenter<NewGameContract.View>(), NewGameContract.Presenter {
 
     var mKey :String = ""
     override fun createGame() {
+        val observer : Observer<String> = object : Observer<String>{
+            override fun onSubscribe(d: Disposable) {
+                Log.d("Subscriber","New Subscriber")
+            }
 
-        gameManager.newGame(Game("miJuego")){ key ->
-            gameManager.addPlayer(key, Player("Mikel2"))
-            gameManager.addPlayer(key, Player("Pablo2"))
-            mKey = key
+            override fun onNext(it: String) {
+                mKey = it
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d("Subscriber",e.message)
+            }
+
+            override fun onComplete() {
+                addPlayer(Player("Mikel"))
+                addPlayer(Player("Pabjimcas"))
+            }
+
         }
+        gameManager.newGame(Game("miJuego")).subscribe(observer)
     }
-    fun initGame(){
-        if (mKey.isNotEmpty()){
-            gameManager.startRound(mKey)
-        }
 
+
+    fun addPlayer(player: Player){
+        val observer  = object : Observer<Boolean>{
+            override fun onNext(t: Boolean) {
+                Log.d("AddPlayer","Juego creado ${t}")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d("Subscriber","New Subscriber")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d("Subscriber",e.message)
+            }
+
+            override fun onComplete() {
+
+            }
+        }
+        gameManager.addPlayer(mKey, player,observer)
+    }
+
+    fun initGame(){
+        gameManager.isGamePrepared(mKey,object : Observer<Boolean>{
+            override fun onComplete() {
+                if (mKey.isNotEmpty()){
+                    gameManager.startRound(mKey)
+                }
+            }
+            override fun onError(e: Throwable) {
+                Log.d("isGamePrepared","Error ${e.message}")
+            }
+            override fun onSubscribe(d: Disposable) {
+                Log.d("isGamePrepared","Subscripcion Juego preparado ${d.isDisposed}")
+            }
+
+            override fun onNext(t: Boolean) {
+                Log.d("isGamePrepared","Juego preparado ${t}")
+            }
+        })
     }
 
 }
